03eeea663bc2fb1f6ecf4c9e4b62c07c2c79f6d6
shujaatsiddiqui <shujaat.siddiqui@10pearls.local>
2021-02-25 14:51:31 +0500

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const [dishes,setDishes] = useState(DISHES);
+  console.log(dishes);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <Navbar dark color="primary">
+      <div className="container">
+        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+      </div>
+    </Navbar>
+    <Menu dishes = {dishes} />
+  </div>
+  
   );
 }
 
