d81b7b2c6f5e008b7a4f142143f5a30f63704719
shujaatsiddiqui <shujaat.siddiqui@10pearls.local>
2021-02-25 15:09:06 +0500

    remove extra code and back to previous state

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 import React, { useState } from 'react';
 
-//test added
 function App() {
 
   const [dishes,setDishes] = useState(DISHES);
@@ -17,7 +16,6 @@ function App() {
         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
       </div>
     </Navbar>
-    <Menu dishes = {dishes} />
     <Menu dishes = {dishes} />
   </div>
   
